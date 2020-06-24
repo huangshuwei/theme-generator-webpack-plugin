f5a2dc6afd0225bf7c06154113d686cfbf0bc51c
huangshuwei <hsw89520@163.com>
2020-06-24 17:44:01 +0800

    add vue example

---
+++ example/vue-theme-switch-demo/src/App.vue
@@ -0,0 +1,13 @@
+<template>
+    <div id="app">
+        <home />
+    </div>
+</template>
+
+<script>
+import home from "./views/home/index";
+export default {
+    components: { home },
+    name: "App"
+};
+</script>
---
+++ example/vue-theme-switch-demo/src/views/home/index.vue
@@ -0,0 +1,29 @@
+<template>
+    <div class="home">
+        <div class="home-top-bar">
+            <div class="left">
+                theme-creator-cli Vue Element UI Example
+            </div>
+            <div class="right">
+                <div class="tool"></div>
+                <div class="link-info"></div>
+            </div>
+        </div>
+        <div class="home-content">
+            <div className="content-item">
+                <div className="content-item-block">
+                    <span className="icon" />
+                    <span className="title">Table</span>
+                </div>
+            </div>
+        </div>
+    </div>
+</template>
+<script>
+import "./index.scss";
+export default {
+    data() {
+        return {};
+    }
+};
+</script>
